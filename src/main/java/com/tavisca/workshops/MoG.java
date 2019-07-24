bdca5fd8d137018b779750df159c615801c8efb0
Jajodia <tjajodia@internal.tavisca.com>
2019-07-24 16:22:05 +0530

    initial Commit

---
+++ src/main/java/com/tavisca/workshops/MoG.java
@@ -0,0 +1,9 @@
+package com.tavisca.workshops;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MoG {
+
+        
+}
---
+++ src/test/java/com/tavisca/workshops/MoGTest.java
@@ -0,0 +1,12 @@
+package com.tavisca.workshops;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MoGTest {
+
+
+
+
+
+
+}
